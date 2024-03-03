5a666ca839e586c74ef8bad01405a720425859ba
mongmeo-dev <mongmeo.dev@gmail.com>
2024-03-03 18:52:42 +0900

    add run server code

--- cmd/http_server/main.go
+++ cmd/http_server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"mongmeo.dev/todo/internal/adapter/database/ent"
+	"net/http"
 	"os"
 )
 
@@ -13,4 +16,12 @@ func main() {
 		log.Fatalf("An exception occurred while connecting to mysql server %s", os.Getenv("DB_DSN"))
 	}
 	defer client.Close()
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	err = http.ListenAndServe(":3000", r)
+	if err != nil {
+		log.Fatal("Fail to start application")
+	}
 }
